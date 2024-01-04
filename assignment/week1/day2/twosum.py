5b49534099d87556540b4c560f6b6e974dcd9acf
Syfur Rahman <43377197+syfurrahman@users.noreply.github.com>
2024-01-04 01:27:11 -0500

    twosum.py

---
+++ assignment/week1/day2/twosum.py
@@ -0,0 +1,16 @@
+# Syfur Rahman
+# Date: Jan 3, 2023
+# Technical interview prep Winter 2024
+# Leetcode - 1 Two Sum
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
