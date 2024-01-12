b3d5ddda9dbcd2e57a7e915e04aa135044ddd3a1
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2024-01-11 21:28:35 -0500

    kth_smallest_bst hw"

---
+++ assignments/week2/day4/kth_smallest_bst.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int result = 0;
+        inorder(root, k, result);
+        return result;
+    }
+private:
+    void inorder(TreeNode* root, int& k, int& result) {
+        if (root == NULL) {
+            return;
+        }
+        inorder(root -> left, k, result);
+        k--;
+        if (k == 0) {
+            result = root -> val;
+            return;
+        }
+        inorder(root -> right, k, result);
+    }
+};
