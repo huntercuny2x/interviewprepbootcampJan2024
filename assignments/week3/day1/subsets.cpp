f470ab60e62ab6368d0d25be79645bfee77aca33
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2024-01-16 18:10:38 -0500

    subsets hw:

---
+++ assignments/week3/day1/subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;
+        vector<vector<int>> result;
+        dfs(nums, 0, curr, result);
+        return result;
+    }
+private:
+    void dfs(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& result) {
+        result.push_back(curr);
+        for (int i = start; i < nums.size(); i++) {
+            curr.push_back(nums[i]);
+            dfs(nums, i + 1, curr, result);
+            curr.pop_back();
+        }
+    }
+};
